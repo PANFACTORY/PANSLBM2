da2eb486ea11b43c163e66401c0a2ed4930eb778
PANFACTORY <yutatanabe510@gmail.com>
2020-10-28 10:20:50 +0900

    Update ibnavierstokes.h

--- src/equation/ibnavierstokes.h
+++ src/equation/ibnavierstokes.h
@@ -7,6 +7,7 @@
 
 
 #pragma once
+#include <cmath>
 #include <cassert>
 
 
@@ -26,19 +27,19 @@ public:
         template<class ...Ts>
         void SetFt(int _i, T _rho, Ts ..._u);
         template<class ...Ts>
-        void SetXk(int _k, Ts ..._xk);
+        void SetXb(int _k, Ts ..._xb);
         template<class ...Ts>
-        void SetUk(int _k, Ts ..._uk);
+        void SetUb(int _k, Ts ..._ub);
 
         T GetRho(int _i) const;
         T GetU(int _d, int _i) const;
         
 private:
-        const int np;                   //  np : number of particle
+        const int np, nb;                               //  np : number of particle, nb : number of boundary point
         T omega;
         P<T>* f;
-        T *rho, *u[P<T>::nd];           //  Macroscopic value
-        T *Xk[P<T>::nd], *Uk[P<T>::nd]; //  Boundary point coordinate and velocity
+        T *rho, *u[P<T>::nd], *G[P<T>::nd];             //  Macroscopic value
+        T *Xb[P<T>::nd], *Ub[P<T>::nd], *Gb[P<T>::nd];  //  Boundary point coordinate and velocity
     };
 
 
@@ -128,8 +129,72 @@ private:
 
     template<class T, template<class>class P>
     void IBNS<T, P>::ExternalForce() {
+        //  Step0：境界点上の体積力を初期化する
+        for (int k = 0; k < this->nb; k++) {
+            for (int d = 0; d < P<T>::nd; d++) {
+                this->Gb[d][k] = T();
+            }
+        }
+
+        for (int l = 0; l < 5; l++) {
+            //  格子点 → 境界点
+            for (int k = 0; k < this->nb; k ++) {
+                //  Step3：格子点上の流速 → 境界点上の流速
+                T vb[P<T>::nd];
+                for (int d = 0; d < P<T>::nd; d++) {
+                    vb[d] = T();
+                }
+                for (int i = 0; i < this->f->nx; i++) {
+                    for (int j = 0; j < this->f->ny; j++) {
+                        int ij = this->f->ny*i + j;
+                        T w = 1.0, rx = fabs(this->Xb[0][k] - i), ry = fabs(this->Xb[1][k] - j);
+                        w *= rx < 1.0 ? (3.0 - 2.0*rx + sqrt(1.0 + 4.0*rx - 4.0*rx*rx))/8.0 : (1.0 <= rx && rx < 2.0 ? (5.0 - 2.0*rx - sqrt(-7.0 + 12.0*rx - 4.0*rx*rx))/8.0 : T());
+                        w *= ry < 1.0 ? (3.0 - 2.0*ry + sqrt(1.0 + 4.0*ry - 4.0*ry*ry))/8.0 : (1.0 <= ry && ry < 2.0 ? (5.0 - 2.0*ry - sqrt(-7.0 + 12.0*ry - 4.0*ry*ry))/8.0 : T());
+                        vb0 += this->u[0][ij]*w*pow(this->f->dx, P<T>::nd);
+                        vb1 += this->u[1][ij]*w*pow(this->f->dx, P<T>::nd);
+                    }
+                }
+
+                //  Step4：境界点上の流速 → 境界点上の体積力
+                for (int d = 0; d < P<T>::nd; d++) {
+                    this->Gb[d][k] += this->f->dx*(this->Ub[d][k] - vb[d]);
+                }
+            }
+
+            //  境界点 → 格子点
+            for (int i = 0; i < this->f->nx; i++) {
+                for (int j = 0; j < this->f->ny; j++) {
+                    int ij = this->f->ny*i + j;
+
+                    //  Step1：境界点上の体積力 → 格子点上の体積力
+                    for (int d = 0; d < P<T>::nd; d++) {
+                        this->G[d][ij] = T();
+                    }
+                    for (int k = 0; k < this->nb; k++) {
+                        T w = 1.0, rx = fabs(this->Xb[0][k] - i), ry = fabs(this->Xb[1][k] - j);
+                        w *= rx < 1.0 ? (3.0 - 2.0*rx + sqrt(1.0 + 4.0*rx - 4.0*rx*rx))/8.0 : (1.0 <= rx && rx < 2.0 ? (5.0 - 2.0*rx - sqrt(-7.0 + 12.0*rx - 4.0*rx*rx))/8.0 : T());
+                        w *= ry < 1.0 ? (3.0 - 2.0*ry + sqrt(1.0 + 4.0*ry - 4.0*ry*ry))/8.0 : (1.0 <= ry && ry < 2.0 ? (5.0 - 2.0*ry - sqrt(-7.0 + 12.0*ry - 4.0*ry*ry))/8.0 : T());
+                        this->G[0][ij] += this->Gb[0][k]*w*pow(this->f->dx, P<T>::nd);
+                        this->G[1][ij] += this->Gb[1][k]*w*pow(this->f->dx, P<T>::nd);
+                    }
+
+                    //  Step2：格子点上の体積力 → 格子点上の流速
+                    for (int d = 0; d < P<T>::nd; d++) {
+                        this->u[d][ij] += this->f->dx*this->G[d][ij];
+                    }
+                }
+            }
+        }
+        
+        //  Step5：速度分布関数を更新する
         for (int i = 0; i < this->np; i++) {
-            
+            for (int j = 0; j < P<T>::nc; j++) {
+                T ciG = T();
+                for (int d = 0; d < P<T>::nd; d++) {
+                    ciG += P<T>::ci[j][d]*this->G[d][i];
+                }
+                this->f->ft[j] += 3.0*this->f->dx*P<T>::ei[j]*ciG;
+            }
         }
     }
 
@@ -152,7 +217,7 @@ private:
 
     template<class T, template<class>class P>
     template<class ...Ts>
-    void IBNS<T, P>::SetXk(int _k, Ts ..._xk) {
+    void IBNS<T, P>::SetXb(int _k, Ts ..._xb) {
         assert(P<T>::nd == sizeof...(Ts));
         
     }
@@ -160,9 +225,9 @@ private:
 
     template<class T, template<class>class P>
     template<class ...Ts>
-    void IBNS<T, P>::SetUk(int _k, Ts ..._uk) {
+    void IBNS<T, P>::SetUb(int _k, Ts ..._ub) {
         assert(P<T>::nd == sizeof...(Ts));
-        
+
     }
 
 

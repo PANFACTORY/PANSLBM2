389e870a23d5c7bf05867d187b6f7db9c6b9128f
PANFACTORY <yutatanabe510@gmail.com>
2021-01-12 11:56:28 +0900

    Update d2q9.h

--- src/particle/d2q9.h
+++ src/particle/d2q9.h
@@ -442,7 +442,7 @@ public:
             this->ft[7][ij] = this->ft[5][ij] + rho0 + flux0;
         } else if (_i == this->nx - 1) {
             T rho0 = -(4.0*this->ft[3][ij] + this->ft[6][ij] + this->ft[7][ij])/3.0;
-            T flux0 = _temperature/(3.0*(1.0 - 3.0*_ux)*_rho)*(4.0*_g.ft[3][ij] + _g.ft[6][ij] + _g.ft[7][ij]) + _uy*_temperature/(2.0*(1.0 - 3.0*_ux)*_rho)*(_g.ft[6][ij] - _g.ft[7][ij]);
+            T flux0 = _temperature/(3.0*(1.0 + 3.0*_ux)*_rho)*(4.0*_g.ft[3][ij] + _g.ft[6][ij] + _g.ft[7][ij]) + _uy*_temperature/(2.0*(1.0 + 3.0*_ux)*_rho)*(_g.ft[6][ij] - _g.ft[7][ij]);
             this->ft[1][ij] = this->ft[3][ij] + rho0 + flux0;
             this->ft[5][ij] = this->ft[7][ij] + rho0 + flux0;
             this->ft[8][ij] = this->ft[6][ij] + rho0 + flux0;
@@ -454,7 +454,7 @@ public:
             this->ft[8][ij] = this->ft[6][ij] + rho0 + flux0;
         } else if (_j == this->ny - 1) {
             T rho0 = -(4.0*this->ft[4][ij] + this->ft[7][ij] + this->ft[8][ij])/3.0;
-            T flux0 = _temperature/(3.0*(1.0 - 3.0*_uy)*_rho)*(4.0*_g.ft[4][ij] + _g.ft[7][ij] + _g.ft[8][ij]) + _ux*_temperature/(2.0*(1.0 - 3.0*_uy)*_rho)*(_g.ft[8][ij] - _g.ft[7][ij]);
+            T flux0 = _temperature/(3.0*(1.0 + 3.0*_uy)*_rho)*(4.0*_g.ft[4][ij] + _g.ft[7][ij] + _g.ft[8][ij]) + _ux*_temperature/(2.0*(1.0 + 3.0*_uy)*_rho)*(_g.ft[8][ij] - _g.ft[7][ij]);
             this->ft[2][ij] = this->ft[4][ij] + rho0 + flux0;
             this->ft[5][ij] = this->ft[7][ij] + rho0 + flux0;
             this->ft[6][ij] = this->ft[8][ij] + rho0 + flux0;

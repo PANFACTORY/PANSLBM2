a5cb33cea63cf102d544d751421b18e55ee43400
PANFACTORY <yutatanabe510@gmail.com>
2021-01-28 10:17:44 +0900

    Update elastic.h

--- src/equation/elastic.h
+++ src/equation/elastic.h
@@ -43,7 +43,7 @@ namespace PANSLBM2 {
         void Collision(T _elasticy, P<T>& _p, T* _rho, T* _ux, T* _uy, T* _sxx, T* _sxy, T* _syx, T* _syy) {
             assert(P<T>::nd == 2);
 //  要修正：緩和時間はelasticyの関数になるはずだがその具体的形が不明のため
-            T omega = 1.0;
+            T omega = 1.0/0.8;
             for (int i = 0; i < _p.np; i++) {
                 for (int j = 0; j < P<T>::nc; j++) {
                     T cu = P<T>::cx[j]*_ux[i] + P<T>::cy[j]*_uy[i];
@@ -62,10 +62,10 @@ namespace PANSLBM2 {
         void InitialCondition(int _i, P<T>& _p, T _rho, T _ux, T _uy, T _sxx, T _sxy, T _syx, T _syy) {
             assert(P<T>::nd == 2 && 0 <= _i && _i < _p.np);
             for (int j = 0; j < P<T>::nc; j++) {
-                T cu = P<T>::cx[j]*_ux[i] + P<T>::cy[j]*_uy[i];
-                T csc = P<T>::cx[j]*_sxx[i]*P<T>::cx[j] + P<T>::cx[j]*_sxy[i]*P<T>::cy[j] + P<T>::cy[j]*_syx[i]*P<T>::cx[j] + P<T>::cy[j]*_syy[i]*P<T>::cy[j];
-                T trs = _sxx[i] + _syy[i];
-                _p.ft[j][_i] = P<T>::ei[j]*(3.0*_rho[i]*cu - 4.5*csc + 1.5*trs);
+                T cu = P<T>::cx[j]*_ux + P<T>::cy[j]*_uy;
+                T csc = P<T>::cx[j]*_sxx*P<T>::cx[j] + P<T>::cx[j]*_sxy*P<T>::cy[j] + P<T>::cy[j]*_syx*P<T>::cx[j] + P<T>::cy[j]*_syy*P<T>::cy[j];
+                T trs = _sxx + _syy;
+                _p.ft[j][_i] = P<T>::ei[j]*(3.0*_rho*cu - 4.5*csc + 1.5*trs);
             }
         }
     }

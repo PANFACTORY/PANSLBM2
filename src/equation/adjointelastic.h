89582ce6d5b9f7495bb509cdd72ff3b092a2750d
PANFACTORY <yutatanabe510@gmail.com>
2021-01-29 09:49:19 +0900

    Update adjointelastic.h

--- src/equation/adjointelastic.h
+++ src/equation/adjointelastic.h
@@ -40,7 +40,7 @@ namespace PANSLBM2 {
         //  Adjoint elastic 2D  :   Collision term
         //*********************************************************************
         template<class T, template<class>class P>
-        void Collision(T _elasticy, P<T>& _p, T* _irho, T* _imx, T* _imy, T* _isxx, T* _isxy, T* _isyx, T* _isyy) {
+        void Collision(T _elasticy, P<T>& _p, T* _irho, T* _imx, T* _imy, T* _isxx, T* _isxy, T* _isyx, T* _isyy, T* _gamma) {
             assert(P<T>::nd == 2);
 //  要修正：緩和時間はelasticyの関数になるはずだがその具体的形が不明のため
             T omega = 1.0/(3.0*_elasticy*_p.dt/(_p.dx*_p.dx) + 0.5);
@@ -49,7 +49,7 @@ namespace PANSLBM2 {
                     T imc = _imx[i]*P<T>::cx[j] + _imy[i]*P<T>::cy[j];
                     T cisc = P<T>::cx[j]*_isxx[i]*P<T>::cx[j] + P<T>::cx[j]*_isxy[i]*P<T>::cy[j] + P<T>::cy[j]*_isyx[i]*P<T>::cx[j] + P<T>::cy[j]*_isyy[i]*P<T>::cy[j];
                     T irhocc = _irho[i]*(P<T>::cx[j]*P<T>::cx[j] + P<T>::cy[j]*P<T>::cy[j]);
-                    T feq = 3.0*imc + 4.5*cisc - 1.5*irhocc;
+                    T feq = 3.0*imc + 4.5*_gamma[i]*cisc - 1.5*_gamma[i]*irhocc;
                     _p.ftp1[j][i] = (1.0 - omega)*_p.ft[j][i] + omega*feq;
                 }
             }
@@ -59,13 +59,13 @@ namespace PANSLBM2 {
         //  Adjoint elastic 2D  :   Initial condition
         //*********************************************************************
         template<class T, template<class>class P>
-        void InitialCondition(int _i, P<T>& _p, T _irho, T _imx, T _imy, T _isxx, T _isxy, T _isyx, T _isyy) {
+        void InitialCondition(int _i, P<T>& _p, T _irho, T _imx, T _imy, T _isxx, T _isxy, T _isyx, T _isyy, T _gamma) {
             assert(P<T>::nd == 2 && 0 <= _i && _i < _p.np);
             for (int j = 0; j < P<T>::nc; j++) {
                 T imc = _imx*P<T>::cx[j] + _imy*P<T>::cy[j];
                 T cisc = P<T>::cx[j]*_isxx*P<T>::cx[j] + P<T>::cx[j]*_isxy*P<T>::cy[j] + P<T>::cy[j]*_isyx*P<T>::cx[j] + P<T>::cy[j]*_isyy*P<T>::cy[j];
                 T irhocc = _irho*(P<T>::cx[j]*P<T>::cx[j] + P<T>::cy[j]*P<T>::cy[j]);
-                _p.ft[j][_i] = 3.0*imc + 4.5*cisc -1.5*irhocc;
+                _p.ft[j][_i] = 3.0*imc + 4.5*_gamma*cisc -1.5*_gamma[i]*irhocc;
             }
         }
     }

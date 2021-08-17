36ae48109c74ee5746cd0e02f879a5973c8e09d8
PANFACTORY <yutatanabe510@gmail.com>
2021-08-17 16:04:40 +0900

    Update cavityflow_mpi.cpp

--- test/cavityflow_mpi.cpp
+++ test/cavityflow_mpi.cpp
@@ -35,15 +35,15 @@ int main(int argc, char** argv) {
     int boundaryu[pf.nxy];
     double uxbc[pf.nbc], uybc[pf.nbc];
     if (MyRank == 0) {
-        pf.SetBoundary([&](int _i, int _j) {    return (_i == 0 || _j == 0) ? 1 : 0; });
+        pf.SetBoundary([&](int _i, int _j) {    return (_i == 0 || _j == 0 || _j == pf.ny - 1) ? 1 : 0; });
         pf.SetBoundary(boundaryu, [&](int _i, int _j) { return _j == pf.ny - 1 ? 1 : 0;    });
         for (int idxbc = 0; idxbc < pf.nbc; ++idxbc) {
             uxbc[idxbc] = u0;
             uybc[idxbc] = 0.0;
         }   
     } else if (MyRank == 1) {
-        pf.SetBoundary([&](int _i, int _j) {    return (_j == 0 || _i == pf.nx - 1) ? 1 : 0; });
-        pf.SetBoundary(boundaryu, [&](int _i, int _j) { return _j == pf.ny - 1 ? 1 : 0;    });
+        pf.SetBoundary([&](int _i, int _j) {    return (_j == 0 || _j == pf.ny - 1) ? 1 : 0; });
+        pf.SetBoundary(boundaryu, [&](int _i, int _j) { return _i == pf.nx - 1 ? 1 : 0;    });
         for (int idxbc = 0; idxbc < pf.nbc; ++idxbc) {
             uxbc[idxbc] = u0;
             uybc[idxbc] = 0.0;

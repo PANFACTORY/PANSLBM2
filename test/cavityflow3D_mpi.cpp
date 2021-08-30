22051e86e0be8b8dba3f6dd8666f70c8eb42e873
PANFACTORY <yutatanabe510@gmail.com>
2021-08-30 15:52:14 +0900

    Update cavityflow3D_mpi.cpp

--- test/cavityflow3D_mpi.cpp
+++ test/cavityflow3D_mpi.cpp
@@ -21,7 +21,7 @@ int main(int argc, char** argv) {
     assert(mx*my*mz == PeTot);
 
     //--------------------Set parameters--------------------
-    int lx = 30, ly = 30, lz = 30, nt = 10, dt = 1;
+    int lx = 30, ly = 30, lz = 30, nt = 100, dt = 10;
     double nu = 0.1, u0 = 0.1, theta = 30.0;
     D3Q15<double> pf(lx, ly, lz, MyRank, mx, my, mz);
     double *rho = new double[pf.nxyz], *ux = new double[pf.nxyz], *uy = new double[pf.nxyz], *uz = new double[pf.nxyz];

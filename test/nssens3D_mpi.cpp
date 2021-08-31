6992bb2e40f00dfad9c1439ec12dae02615088a4
PANFACTORY <yutatanabe510@gmail.com>
2021-08-31 16:18:36 +0900

    Update nssens3D_mpi.cpp

--- test/nssens3D_mpi.cpp
+++ test/nssens3D_mpi.cpp
@@ -117,15 +117,15 @@ int main(int argc, char** argv) {
         [&](int _i, int _j, int _k) { return uy[pf.Index(_i, _j, _k)]; },
         [&](int _i, int _j, int _k) { return uz[pf.Index(_i, _j, _k)]; }
     );
-    file.AddPointScaler("dfds", [&](int _i, int _j, int _k) {   return dfds[pf.Index(_i, _j, _k)];  });
+    //file.AddPointScaler("dfds", [&](int _i, int _j, int _k) {   return dfds[pf.Index(_i, _j, _k)];  });
     file.AddPointScaler("ip", [&](int _i, int _j, int _k) { return irho[pf.Index(_i, _j, _k)]; });
     file.AddPointVector("iu", 
         [&](int _i, int _j, int _k) { return iux[pf.Index(_i, _j, _k)]; },
         [&](int _i, int _j, int _k) { return iuy[pf.Index(_i, _j, _k)]; },
         [&](int _i, int _j, int _k) { return iuz[pf.Index(_i, _j, _k)]; }
     );
-    file.AddPointScaler("alpha", [&](int _i, int _j, int _k) { return alpha[pf.Index(_i, _j, _k)]; });
-    file.AddPointScaler("s", [&](int _i, int _j, int _k) { return s[pf.Index(_i, _j, _k)]; });
+    //file.AddPointScaler("alpha", [&](int _i, int _j, int _k) { return alpha[pf.Index(_i, _j, _k)]; });
+    //file.AddPointScaler("s", [&](int _i, int _j, int _k) { return s[pf.Index(_i, _j, _k)]; });
     
     delete[] rho, ux, uy, uz, irho, iux, iuy, iuz, imx, imy, imz, s, alpha, dfds;
     delete[] boundaryup, uxbc, uybc, uzbc, rhobc, usbc, utbc;

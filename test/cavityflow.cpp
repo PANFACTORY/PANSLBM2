084dde68173e3ee2676029edf2103b7e3cd37237
PANFACTORY <yutatanabe510@gmail.com>
2021-09-08 21:03:54 +0900

    Update cavityflow.cpp

--- test/cavityflow.cpp
+++ test/cavityflow.cpp
@@ -27,7 +27,7 @@ int main(int argc, char** argv) {
 #endif
 
     //--------------------Set parameters--------------------
-    int lx = 101, ly = 101, nt = 10000, dt = 100;
+    int lx = 101, ly = 101, nt = 10, dt = 1;
     double nu = 0.1, u0 = 0.1, Re = u0*(lx - 1)/nu;
     D2Q9<double> pf(lx, ly, MyRank, mx, my);
     double rho[pf.nxy], ux[pf.nxy], uy[pf.nxy];

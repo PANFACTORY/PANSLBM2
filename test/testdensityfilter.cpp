dc89d602abbdef9304c5c06158aa80a66a63524e
PANFACTORY <yutatanabe510@gmail.com>
2021-09-13 10:17:53 +0900

    Update testdensityfilter.cpp

--- test/testdensityfilter.cpp
+++ test/testdensityfilter.cpp
@@ -7,7 +7,7 @@
     #include "mpi.h"
 #endif
 
-#include "../src/particle/d2q9.h"
+#include "../src/particle/d3q15.h"
 #include "../src/utility/densityfilter.h"
 #include "../src/utility/vtkxmlexport.h"
 
@@ -20,27 +20,30 @@ int main(int argc, char** argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &PeTot);
     MPI_Comm_rank(MPI_COMM_WORLD, &MyRank);
 
-    assert(argc == 3);
-    int mx = atoi(argv[1]), my = atoi(argv[2]);
-    assert(mx*my == PeTot);
+    assert(argc == 4);
+    int mx = atoi(argv[1]), my = atoi(argv[2]), mz = atoi(argv[3]);
+    assert(mx*my*mz == PeTot);
 #else
-    int MyRank = 0, mx = 1, my = 1;
+    int MyRank = 0, mx = 1, my = 1, mz = 1;
 #endif
 
-    int lx = 101, ly = 101;
+    int lx = 51, ly = 51, lz = 51;
     double R = 1.5;
-    D2Q9<double> pf(lx, ly, MyRank, mx, my);
+    D3Q15<double> pf(lx, ly, lz, MyRank, mx, my, mz);
     std::vector<double> v(pf.nxyz);
     for (int i = 0; i < pf.nx; ++i) {
         for (int j = 0; j < pf.ny; ++j) {
-            int idx = pf.Index(i, j);
-            v[idx] = (i + pf.offsetx)%2 == (j + pf.offsety)%2 ? 1.0 : 0.0;
+            for (int k = 0; k < pf.nz; ++k) {
+                int idx = pf.Index(i, j, k);
+                v[idx] = ((i + pf.offsetx) + (j + pf.offsety) + (k + pf.offsetz))%2 ? 1.0 : 0.0;
+            }
+            
         }
     }
     std::vector<double> fv = DensityFilter::GetFilteredValue(pf, 1.5, v);
-    VTKXMLExport file("result/densityfilter", MyRank, lx, ly, 1, mx, my, 1);
-    file.AddPointScaler("v", [&](int _i, int _j, int _k) { return v[pf.Index(_i, _j)]; });
-    file.AddPointScaler("fv", [&](int _i, int _j, int _k) { return fv[pf.Index(_i, _j)]; });
+    VTKXMLExport file("result/densityfilter", MyRank, lx, ly, lz, mx, my, mz);
+    file.AddPointScaler("v", [&](int _i, int _j, int _k) { return v[pf.Index(_i, _j, _k)]; });
+    file.AddPointScaler("fv", [&](int _i, int _j, int _k) { return fv[pf.Index(_i, _j, _k)]; });
 #ifdef _USE_MPI_DEFINES
     MPI_Finalize();
 #endif

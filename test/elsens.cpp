cc3f1e057d1a98d0a48ce81293277ce511fad024
PANFACTORY <yutatanabe510@gmail.com>
2021-09-15 08:37:13 +0900

    Update elsens.cpp

--- test/elsens.cpp
+++ test/elsens.cpp
@@ -1,6 +1,10 @@
+#define _USE_MPI_DEFINES
 #include <iostream>
 #include <cmath>
 #include <chrono>
+#ifdef _USE_MPI_DEFINES
+    #include "mpi.h"
+#endif
 
 #include "../src/particle/d2q9.h"
 #include "../src/equation/elastic.h"
@@ -9,18 +13,31 @@
 
 using namespace PANSLBM2;
 
-int main() {
+int main(int argc, char** argv) {
+#ifdef _USE_MPI_DEFINES
+    int PeTot, MyRank;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &PeTot);
+    MPI_Comm_rank(MPI_COMM_WORLD, &MyRank);
+
+    assert(argc == 3);
+    int mx = atoi(argv[1]), my = atoi(argv[2]);
+    assert(mx*my == PeTot);
+#else
+    int MyRank = 0, mx = 1, my = 1; 
+#endif
+
     //--------------------Setting parameters--------------------
-    int nx = 81, ny = 61, nt = 10000, dt = 1000;
+    int lx = 81, ly = 61, nt = 10000, dt = 1000;
     double rho0 = 1e6, stress0 = -1.0, p = 0.25, smin = 0.1, smax = 0.9;
-    D2Q9<double> pf(nx, ny);
+    D2Q9<double> pf(lx, ly, MyRank, mx, my);
     double rho[pf.nxyz], ux[pf.nxyz], uy[pf.nxyz], rx[pf.nxyz], ry[pf.nxyz], sxx[pf.nxyz], sxy[pf.nxyz], syx[pf.nxyz], syy[pf.nxyz];
     double irho[pf.nxyz], imx[pf.nxyz], imy[pf.nxyz], isxx[pf.nxyz], isxy[pf.nxyz], isyx[pf.nxyz], isyy[pf.nxyz];
     double s[pf.nxyz], gamma[pf.nxyz], sensitivity[pf.nxyz];
     for (int i = 0; i < pf.nx; ++i) {
         for (int j = 0; j < pf.ny; ++j) {
             int idx = pf.Index(i, j);
-            s[idx] = pow(i - 0.5*pf.nx, 2.0) + pow(j, 2.0) < pow(0.15*pf.nx, 2.0) ? smin : smax;
+            s[idx] = pow((i + _p.offsetx) - 0.5*pf.lx, 2.0) + pow((j + _p.offsety), 2.0) < pow(0.15*pf.lx, 2.0) ? smin : smax;
         }
     }
     for (int idx = 0; idx < pf.nxyz; ++idx) {
@@ -43,7 +60,7 @@ int main() {
         pf.Swap();
         pf.BoundaryCondition([=](int _i, int _j) { return _i == 0 ? 1 : 0; });
         EL::BoundaryConditionSetStress(pf, 
-            [=](int _i, int _j) { return (_i == nx - 1 && fabs(_j - 0.5*ny) < 0.08*ny) ? stress0 : 0.0; }, 
+            [=](int _i, int _j) { return (_i == lx - 1 && fabs(_j - 0.5*ly) < 0.08*ly) ? stress0 : 0.0; }, 
             [=](int _i, int _j) { return 0.0; }, 
             [=](int _i, int _j) { return _i != 0; }
         );
@@ -61,7 +78,7 @@ int main() {
         pf.Swap();
         pf.iBoundaryCondition([=](int _i, int _j) { return _i == 0 ? 1 : 0; });
         AEL::BoundaryConditionSetiStress(pf, 
-            [=](int _i, int _j) { return (_i == nx - 1 && fabs(_j - 0.5*ny) < 0.08*ny) ? stress0 : 0.0; }, 
+            [=](int _i, int _j) { return (_i == lx - 1 && fabs(_j - 0.5*ly) < 0.08*ly) ? stress0 : 0.0; }, 
             [=](int _i, int _j) { return 0.0; }, 
             rho, 
             [=](int _i, int _j) { return _i != 0; }
@@ -82,18 +99,18 @@ int main() {
     }
 
     //--------------------Export result--------------------
-    VTKExport file("result/adjointelastic.vtk", nx, ny);
-    file.AddPointVector("u", 
+    VTKXMLExport file(pf, "result/adjointelastic");
+    file.AddPointData(pf, "u", 
         [&](int _i, int _j, int _k) { return ux[pf.Index(_i, _j)]; },
         [&](int _i, int _j, int _k) { return uy[pf.Index(_i, _j)]; },
         [](int _i, int _j, int _k) { return 0.0; }
     );
-    file.AddPointVector("r", 
+    file.AddPointData(pf, "r", 
         [&](int _i, int _j, int _k) { return rx[pf.Index(_i, _j)]; },
         [&](int _i, int _j, int _k) { return ry[pf.Index(_i, _j)]; },
         [](int _i, int _j, int _k) { return 0.0; }
     );
-    file.AddPointTensor("s", 
+    file.AddPointData(pf, "s", 
         [&](int _i, int _j, int _k) { return sxx[pf.Index(_i, _j)]; },
         [&](int _i, int _j, int _k) { return sxy[pf.Index(_i, _j)]; },
         [](int _i, int _j, int _k) { return 0.0; },
@@ -104,13 +121,13 @@ int main() {
         [](int _i, int _j, int _k) { return 0.0; },
         [](int _i, int _j, int _k) { return 0.0; }
     );
-    file.AddPointScaler("irho", [&](int _i, int _j, int _k) { return irho[pf.Index(_i, _j)]; });
-    file.AddPointVector("iu", 
+    file.AddPointData(pf, "irho", [&](int _i, int _j, int _k) { return irho[pf.Index(_i, _j)]; });
+    file.AddPointData(pf, "iu", 
         [&](int _i, int _j, int _k) { return imx[pf.Index(_i, _j)]; },
         [&](int _i, int _j, int _k) { return imy[pf.Index(_i, _j)]; },
         [](int _i, int _j, int _k) { return 0.0; }
     );
-    file.AddPointTensor("is", 
+    file.AddPointData(pf, "is", 
         [&](int _i, int _j, int _k) { return isxx[pf.Index(_i, _j)]; },
         [&](int _i, int _j, int _k) { return isxy[pf.Index(_i, _j)]; },
         [](int _i, int _j, int _k) { return 0.0; },
@@ -121,12 +138,16 @@ int main() {
         [](int _i, int _j, int _k) { return 0.0; },
         [](int _i, int _j, int _k) { return 0.0; }
     );
-    file.AddPointScaler("sensitivity", [&](int _i, int _j, int _k) { return sensitivity[pf.Index(_i, _j)]; });
-    file.AddPointScaler("gamma", [&](int _i, int _j, int _k) { return gamma[pf.Index(_i, _j)]; });
-    file.AddPointScaler("ss", [&](int _i, int _j, int _k) { return s[pf.Index(_i, _j)]; });
+    file.AddPointData(pf, "sensitivity", [&](int _i, int _j, int _k) { return sensitivity[pf.Index(_i, _j)]; });
+    file.AddPointData(pf, "gamma", [&](int _i, int _j, int _k) { return gamma[pf.Index(_i, _j)]; });
+    file.AddPointData(pf, "ss", [&](int _i, int _j, int _k) { return s[pf.Index(_i, _j)]; });
 
     std::chrono::system_clock::time_point end = std::chrono::system_clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count() << std::endl;
-
+    if (MyRank == 0) {
+        std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count() << std::endl;
+    }
+#ifdef _USE_MPI_DEFINES
+    MPI_Finalize();
+#endif
     return 0;
 }

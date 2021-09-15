bb20049bf17ebe41fd8e715a24dcba79d2164bec
PANFACTORY <yutatanabe510@gmail.com>
2021-09-15 09:47:57 +0900

    Update residual.h

--- src/utility/residual.h
+++ src/utility/residual.h
@@ -6,7 +6,7 @@
 
 namespace PANSLBM2 {
     template<class T>
-    T residual(const T *_ux, const T *_uy, const T *_uxp, const T *_uyp, int _nxy) {
+    T Residual(const T *_ux, const T *_uy, const T *_uxp, const T *_uyp, int _nxy) {
         T norm_buffer[2] = { T() }, norm[2] = { T() };
         for (int idx = 0; idx < _nxy; ++idx) {
             norm_buffer[0] += pow(_ux[idx] - _uxp[idx], 2.0) + pow(_uy[idx] - _uyp[idx], 2.0);

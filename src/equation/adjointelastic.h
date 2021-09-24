2fb62c562265e1a60a7cad1a14b030e91ff0e708
PANFACTORY <yutatanabe510@gmail.com>
2021-09-25 08:29:04 +0900

    Update adjointelastic.h

--- src/equation/adjointelastic.h
+++ src/equation/adjointelastic.h
@@ -12,15 +12,15 @@ namespace PANSLBM2 {
     namespace AEL {
         //  Function of updating macroscopic values of AEL for 2D
         template<class T, template<class>class P>
-        void Macro(T &_irho, T &_imx, T &_imy, T &_isxx, T &_isxy, T &_isyx, T &_isyy, const T *_f, int _idx) {
-            _irho = T();
+        void Macro(T &_irho, T &_imx, T &_imy, T &_isxx, T &_isxy, T &_isyx, T &_isyy, const T *_f0, const T *_f, int _idx) {
+            _irho = P<T>::ei[0]*_f0[_idx];
             _imx = T();
             _imy = T();
             _isxx = T();
             _isxy = T();
             _isyx = T();
             _isyy = T();
-            for (int c = 0; c < P<T>::nc; ++c) {
+            for (int c = 1; c < P<T>::nc; ++c) {
                 _irho += P<T>::ei[c]*_f[P<T>::IndexF(_idx, c)];
                 _imx += P<T>::ei[c]*P<T>::cx[c]*_f[P<T>::IndexF(_idx, c)];
                 _imy += P<T>::ei[c]*P<T>::cy[c]*_f[P<T>::IndexF(_idx, c)];
@@ -42,34 +42,29 @@ namespace PANSLBM2 {
 
         //  Function of Update macro, Collide and Stream of AEL for 2D
         template<class T, template<class>class P>
-        void MacroCollideStream(
-            P<T>& _p, T *_irho, T *_imx, T *_imy, T *_isxx, T *_isxy, T *_isyx, T *_isyy, T _tau, const T *_gamma, bool _issave = false
-        ) {
+        void MacroCollideStream(P<T>& _p, T *_irho, T *_imx, T *_imy, T *_isxx, T *_isxy, T *_isyx, T *_isyy, T _tau, const T *_gamma, bool _issave = false) {
             T omega = 1/_tau;
-            for (int i = 0; i < _p.nx; ++i) {
-                for (int j = 0; j < _p.ny; ++j) {
-                    int idx = _p.Index(i, j);
+#pragma omp parallel for
+            for (int idx = 0; idx < _p.nxyz; ++idx) {
+                //  Update macro
+                T irho, imx, imy, isxx, isxy, isyx, isyy;
+                Macro<T, P>(irho, imx, imy, isxx, isxy, isyx, isyy, _p.f0, _p.f, idx);
 
-                    //  Update macro
-                    T irho, imx, imy, isxx, isxy, isyx, isyy;
-                    Macro<T, P>(irho, imx, imy, isxx, isxy, isyx, isyy, _p.f, idx);
+                //  Save macro if need
+                if (_issave) {
+                    _irho[idx] = irho;
+                    _imx[idx] = imx;
+                    _imy[idx] = imy;
+                    _isxx[idx] = isxx;
+                    _isxy[idx] = isxy;
+                    _isyx[idx] = isyx;
+                    _isyy[idx] = isyy;
+                }
 
-                    //  Save macro if need
-                    if (_issave) {
-                        _irho[idx] = irho;
-                        _imx[idx] = imx;
-                        _imy[idx] = imy;
-                        _isxx[idx] = isxx;
-                        _isxy[idx] = isxy;
-                        _isyx[idx] = isyx;
-                        _isyy[idx] = isyy;
-                    }
-
-                    //  Collide and stream
-                    for (int c = 0; c < P<T>::nc; ++c) {
-                        int idxstream = _p.Index(i - P<T>::cx[c], j - P<T>::cy[c]);
-                        _p.fnext[P<T>::IndexF(idxstream, c)] = (1.0 - omega)*_p.f[P<T>::IndexF(idx, c)] + omega*Equilibrium<T, P>(irho, imx, imy, isxx, isxy, isyx, isyy, _gamma[idx], c);
-                    }
+                //  Collide
+                _p.f0[idx] = (1.0 - omega)*_p.f0[idx] + omega*Equilibrium<T, P>(irho, imx, imy, isxx, isxy, isyx, isyy, _gamma[idx], 0);
+                for (int c = 1; c < P<T>::nc; ++c) {
+                    _p.f[P<T>::IndexF(idx, c)] = (1.0 - omega)*_p.f[P<T>::IndexF(idx, c)] + omega*Equilibrium<T, P>(irho, imx, imy, isxx, isxy, isyx, isyy, _gamma[idx], c);
                 }
             }
         }
@@ -78,7 +73,8 @@ namespace PANSLBM2 {
         template<class T, template<class>class P>
         void InitialCondition(P<T>& _p, const T *_irho, const T *_imx, const T *_imy, const T *_isxx, const T *_isxy, const T *_isyx, const T *_isyy, const T *_gamma) {
             for (int idx = 0; idx < _p.nxy; ++idx) {
-                for (int c = 0; c < P<T>::nc; ++c) {
+                _p.f0[idx] = Equilibrium<T, P>(_irho[idx], _imx[idx], _imy[idx], _isxx[idx], _isxy[idx], _isyx[idx], _isyy[idx], _gamma[idx], 0);
+                for (int c = 1; c < P<T>::nc; ++c) {
                     _p.f[P<T>::IndexF(idx, c)] = Equilibrium<T, P>(_irho[idx], _imx[idx], _imy[idx], _isxx[idx], _isxy[idx], _isyx[idx], _isyy[idx], _gamma[idx], c);
                 }
             }

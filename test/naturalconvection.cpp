ca38445daadf34c8eea32c92b2549dd38ad1c233
PANFACTORY <yutatanabe510@gmail.com>
2021-09-26 12:38:54 +0900

    Update naturalconvection.cpp

--- test/naturalconvection.cpp
+++ test/naturalconvection.cpp
@@ -44,7 +44,7 @@ int main(int argc, char** argv) {
         if (t%dt == 0 && MyRank == 0) {
             std::cout << "t = " << t/dt << std::endl;
         }
-        AD::MacroCollideStreamNaturalConvection(
+        AD::MacroCollideNaturalConvection(
             pf, rho, ux, uy, nu,
             pg, tem, qx, qy, alpha, 
             0.0, 1.6e-5, 0.5*(Th + Tl), true

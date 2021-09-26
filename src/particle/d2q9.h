916bcb3f4eacce4e766494025f0a84fbe8409888
PANFACTORY <yutatanabe510@gmail.com>
2021-09-26 12:14:39 +0900

    Update d2q9.h

--- src/particle/d2q9.h
+++ src/particle/d2q9.h
@@ -34,10 +34,15 @@ public:
             offsetz(0)
         {
             assert(0 < _lx && 0 < _ly && 0 <= _PEid && 0 < _mx && 0 < _my);
-            
+#ifdef _USE_AVX_DEFINES 
             this->f0 = new(std::align_val_t{32}) T[(this->nxyz/D2Q9<T>::packsize + 1)*D2Q9<T>::packsize];
             this->f = new(std::align_val_t{32}) T[(this->nxyz/D2Q9<T>::packsize + 1)*D2Q9<T>::packsize*(D2Q9<T>::nc - 1)];
             this->fnext = new(std::align_val_t{32}) T[(this->nxyz/D2Q9<T>::packsize + 1)*D2Q9<T>::packsize*(D2Q9<T>::nc - 1)];
+#else
+            this->f0 = new T[this->nxyz];
+            this->f = new T[this->nxyz*(D2Q9<T>::nc - 1)];
+            this->fnext = new T[this->nxyz*(D2Q9<T>::nc - 1)];
+#endif
             this->fsend_xmin = new T[this->ny*3];
             this->fsend_xmax = new T[this->ny*3];
             this->fsend_ymin = new T[this->nx*3];
@@ -71,7 +76,11 @@ public:
             return this->Index(_i, _j);
         }
         static int IndexF(int _idx, int _c) {
+#ifdef _USE_AVX_DEFINES
             return (_idx/D2Q9<T>::packsize)*D2Q9<T>::packsize*(D2Q9<T>::nc - 1) + D2Q9<T>::packsize*(_c - 1) + _idx%D2Q9<T>::packsize;
+#else
+            return (D2Q9<T>::nc - 1)*_idx + (_c - 1);
+#endif
         }
         int IndexPE(int _i, int _j) const {
             int i = _i == -1 ? this->mx - 1 : (_i == this->mx ? 0 : _i);
@@ -98,8 +107,6 @@ public:
 #ifdef _USE_AVX_DEFINES
         static const int packsize = 32/sizeof(T);
         static __m256d __cx[nc], __cy[nc], __cz[nc], __ei[nc];
-#else
-        static const int packsize = 1;
 #endif
 
 private:

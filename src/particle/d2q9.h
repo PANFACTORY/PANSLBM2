4dc6bfece8c131dfd27ed019c245143275dd0dd1
PANFACTORY <yutatanabe510@gmail.com>
2021-10-07 15:05:10 +0900

    Update d2q9.h

--- src/particle/d2q9.h
+++ src/particle/d2q9.h
@@ -34,10 +34,15 @@ public:
             offsetz(0)
         {
             assert(0 < _lx && 0 < _ly && 0 <= _PEid && 0 < _mx && 0 < _my);
-
-            this->f0 = new (std::align_val_t{32}) T[this->nxyz];
-            this->f = new (std::align_val_t{32}) T[this->nxyz*(D2Q9<T>::nc - 1)];
-            this->fnext = new (std::align_val_t{32}) T[this->nxyz*(D2Q9<T>::nc - 1)];
+#ifdef _USE_AVX_DEFINES
+            this->f0 = (T*)_mm_malloc(sizeof(T)*this->nxyz, 32);
+            this->f = (T*)_mm_malloc(sizeof(T)*this->nxyz*(D2Q9<T>::nc - 1), 32);
+            this->fnext = (T*)_mm_malloc(sizeof(T)*this->nxyz*(D2Q9<T>::nc - 1), 32);
+#else
+            this->f0 = new T[this->nxyz];
+            this->f = new T[this->nxyz*(D2Q9<T>::nc - 1)];
+            this->fnext = new T[this->nxyz*(D2Q9<T>::nc - 1)];
+#endif
             this->fsend_xmin = new T[this->ny*3];
             this->fsend_xmax = new T[this->ny*3];
             this->fsend_ymin = new T[this->nx*3];
@@ -52,9 +57,15 @@ public:
         }
         D2Q9(const D2Q9<T>& _p) = delete;
         ~D2Q9() {
+#ifdef _USE_AVX_DEFINES
+            _mm_free(this->f0);
+            _mm_free(this->f);
+            _mm_free(this->fnext);
+#else
             delete[] this->f0;
             delete[] this->f;
             delete[] this->fnext;
+#endif
             delete[] this->fsend_xmin;
             delete[] this->fsend_xmax;
             delete[] this->fsend_ymin;

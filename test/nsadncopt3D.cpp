851505071b8bc460633ab2d27700fcc3ea3dd1cf
PANFACTORY <yutatanabe510@gmail.com>
2021-10-09 11:46:35 +0900

    Update nsadncopt3D.cpp

--- test/nsadncopt3D.cpp
+++ test/nsadncopt3D.cpp
@@ -34,9 +34,9 @@ int main(int argc, char** argv) {
 #endif
 
     //********************Parameters********************
-    int lx = 41, ly = 81, lz = 41, mx = 31, my = 61, mz = 31, nt = 30000, dt = 100, nitr = 2000, nb = 100;
-    double Pr = 6.0, Ra = 2.5e3, nu = 0.1, L = 8.0, tem0 = 0.0, qn0 = 1.0e-2, alphamax = 1.0e4;
-    double qf = 1e-2, qg = 1e0, movelimit = 0.2, weightlimit = 0.05, R = 1.5, eps = 1.0e-4, s0 = 0.05;
+    int lx = 81, ly = 161, lz = 81, mx = 61, my = 121, mz = 61, nt = 30000, dt = 100, nitr = 2000, nb = 100;
+    double Pr = 6.0, Ra = 2.5e4, nu = 0.1, L = 8.0, tem0 = 0.0, qn0 = 1.0e-2, alphamax = 1.0e4;
+    double qf = 1e-2, qg = 1e0, movelimit = 0.2, weightlimit = 0.05, R = 1.5, eps = 1.0e-5, s0 = 0.05;
 
     double U = nu*sqrt(Ra/Pr)/(double)(ly - 1), diff_fluid = nu/Pr, diff_solid = diff_fluid*10.0, gx = 0.0, gy = U*U/(double)(ly - 1), gz = 0.0;
     D3Q15<double> pf(lx, ly, lz, MyRank, nPEx, nPEy, nPEz), pg(lx, ly, lz, MyRank, nPEx, nPEy, nPEz);

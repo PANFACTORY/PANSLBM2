eb95d0d1f2d1ca1d0e4592d5c74cd8d2490ee4ab
PANFACTORY <yutatanabe510@gmail.com>
2021-10-24 07:49:26 +0900

    Update adjointelastic.h

--- src/equation/adjointelastic.h
+++ src/equation/adjointelastic.h
@@ -150,7 +150,7 @@ namespace PANSLBM2 {
         template<class T, template<class>class P>
         void SensitivityCompliance(P<T>& _p, T *_dfds, const T *_sxx, const T *_sxy, const T *_syx, const T *_syy, const T *_irho, const T *_isxx, const T *_isxy, const T *_isyx, const T *_isyy) {
             for (int idx = 0; idx < _p.nxyz; ++idx) {
-                _dfds[idx] = (4.5*(_sxx[idx]*_isxx[idx] + _sxy[idx]*_isxy[idx] + _syx[idx]*_isyx[idx] + _syy[idx]*_isyy[idx]) - 1.5*_irho[idx]*(_sxx[idx] + _syy[idx]));
+                _dfds[idx] += (4.5*(_sxx[idx]*_isxx[idx] + _sxy[idx]*_isxy[idx] + _syx[idx]*_isyx[idx] + _syy[idx]*_isyy[idx]) - 1.5*_irho[idx]*(_sxx[idx] + _syy[idx]));
             }
         }
     }

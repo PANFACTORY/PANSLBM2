58b5240a96460a770f3f730fc504a5b720e21fd9
PANFACTORY <yutatanabe510@gmail.com>
2021-10-24 09:46:26 +0900

    Create normalize.h

---
+++ src/utility/normalize.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <cmath>
+#ifdef _USE_MPI_DEFINES
+    #include "mpi.h"
+#endif
+
+namespace PANSLBM2 {
+    template<class T>
+    void Normalize(T *_v, int _size) {
+        T vmax_buffer = 0.0, vmax;
+        for (int idx = 0; idx < _size; ++idx) {
+            if (vmax_buffer < fabs(_v[idx])) {
+                vmax_buffer = fabs(_v[idx]);
+            }
+        }
+#ifdef _USE_MPI_DEFINES
+        MPI_Allreduce(&vmax_buffer, &vmax, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+#else
+        vmax = vmax_buffer;
+#endif
+        for (int idx = 0; idx < _size; ++idx) {
+            _v[idx] /= vmax;
+        }
+    }
+}

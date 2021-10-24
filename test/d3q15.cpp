f7bdd65f034e7e13ff48e0777fc64467d58508d1
PANFACTORY <yutatanabe510@gmail.com>
2021-10-24 09:28:28 +0900

    Update d3q15.cpp

--- test/d3q15.cpp
+++ test/d3q15.cpp
@@ -1,29 +1,29 @@
 #define _USE_AVX_DEFINES
 #include <stdio.h>
+#include <cmath>
 #include "../src/particle/d3q15.h"
 
 using namespace PANSLBM2;
 
 int main() {
-    //  Test ShuffleToSoA
-    alignas(32) double f[56] = { 
-          0.1,   0.2,   0.3,   0.4,   0.5,   0.6,   0.7,   0.8,   0.9,    1.0,    1.1,    1.2,    1.3,    1.4,
-         10.0,  20.0,  30.0,  40.0,  50.0,  60.0,  70.0,  80.0,  90.0,  100.0,  110.0,  120.0,  130.0,  140.0,
-         -0.1,  -0.2,  -0.3,  -0.4,  -0.5,  -0.6,  -0.7,  -0.8,  -0.9,   -1.0,   -1.1,   -1.2,   -1.3,   -1.4,
-        -10.0, -20.0, -30.0, -40.0, -50.0, -60.0, -70.0, -80.0, -90.0, -100.0, -110.0, -120.0, -130.0, -140.0
-    };
-    __m256d __f[14];
-    D3Q15<double>::ShuffleToSoA(f, __f);
-    for (int c = 0; c < 14; ++c) {
+    D3Q15<double> p(2, 2);
+    for (int idx = 0; idx < p.nxyz; ++idx) {
+        p.f0[idx] = pow(100, idx);
+        for (int c = 1; c < p.nc; ++c) {
+            p.f[p.IndexF(idx, c)] = pow(100, idx) + c;
+        }
+    }
+    __m256d __f[p.nc];
+    p.LoadF(0, __f);
+    for (int c = 0; c < p.nc; ++c) {
         printf("%lf %lf %lf %lf\n", __f[c][3], __f[c][2], __f[c][1], __f[c][0]);
     }
-
-    //  Test ShuffleToAoS
-    D3Q15<double>::ShuffleToAoS(f, __f);
-    for (int idx = 0; idx < 4; ++idx) {
-        printf("%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf\n", 
-            f[14*idx +  0], f[14*idx +  1], f[14*idx +  2], f[14*idx +  3], f[14*idx +  4], f[14*idx +  5], f[14*idx +  6], 
-            f[14*idx +  7], f[14*idx +  8], f[14*idx +  9], f[14*idx + 10], f[14*idx + 11], f[14*idx + 12], f[14*idx + 13]
-        );
+    p.StoreF(0, __f);
+    for (int idx = 0; idx < p.nxyz; ++idx) {
+        printf("%lf", p.f0[idx]);
+        for (int c = 1; c < p.nc; ++c) {
+            printf(" %lf", p.f[p.IndexF(idx, c)]);
+        }
+        printf("\n");
     }
 }

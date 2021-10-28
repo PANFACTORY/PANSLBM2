98df04153fcdc06b7b55e63f37f9d68d789ee049
PANFACTORY <yutatanabe510@gmail.com>
2021-10-28 20:43:25 +0900

    Update heatsink.cpp

--- production/heatsink.cpp
+++ production/heatsink.cpp
@@ -1,8 +1,8 @@
-#define _USE_MATH_DEFINES
-//#define _USE_MPI_DEFINES
 #define _USE_AVX_DEFINES
-#include <cmath>
 #include <iostream>
+#include <chrono>
+#include <vector>
+#include <utility>
 #include <iomanip>
 #ifdef _USE_MPI_DEFINES
     #include "mpi.h"
@@ -31,37 +31,45 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &MyRank);
 
     assert(argc == 3);
-    int mx = atoi(argv[1]), my = atoi(argv[2]);
-    assert(mx*my == PeTot);
+    int nPEx = atoi(argv[1]), nPEy = atoi(argv[2]);
+    assert(nPEx*nPEy == PeTot);
 #else
-    int MyRank = 0, mx = 1, my = 1; 
+    int MyRank = 0, nPEx = 1, nPEy = 1;
 #endif
 
-    //--------------------Set parameters--------------------
-    int lx = 101, ly = 201, nt0 = 0, dt = 1000, nt = 100000, period = 5000, nk = 2000, nb = 100;
-    double viscosity = 0.1/6.0, diff_fluid = viscosity/1.0, Th = 1.0, Tl = 0.0, gx = 0.0, gy = 1/3.6e6;//1/4.5e5;
-    double alphamax = 1e5, diff_solid = diff_fluid*10.0, qf = 1e-6, qg = 1e-4, weightlimit = 0.5, movelimit = 0.2, R = 1.5, eps = 1e-5;
-    D2Q9<double> pf(lx, ly, MyRank, mx, my), pg(lx, ly, MyRank, mx, my);
+    //********************Parameters********************
+    int lx = 141, ly = 161, mx = 81, my = 101, nt = 100000, dt = 100, nk = 2000, nb = 100;
+    double Pr = 6.0, Ra = 2.5e5, nu = 0.1, L = 4.0, tem0 = 0.0, qn = 1.0e-2, alphamax = 1.0e4;
+    double qf = 1e-2, qg = 1e0, movelimit = 0.2, weightlimit = 0.5, R = 1.5, eps = 1.0e-5, s0 = 0.5;
+
+    double U = nu*sqrt(Ra/Pr)/(double)(ly - 1), diff_fluid = nu/Pr, diff_solid = diff_fluid*10.0, gx = 0.0, gy = U*U/(double)(ly - 1);
+    D2Q9<double> pf(lx, ly, MyRank, nPEx, nPEy), pg(lx, ly, MyRank, nPEx, nPEy);
     double *rho = new double[pf.nxyz], *ux = new double[pf.nxyz], *uy = new double[pf.nxyz], *uxp = new double[pf.nxyz], *uyp = new double[pf.nxyz];
     double *tem = new double[pg.nxyz], *qx = new double[pf.nxyz], *qy = new double[pf.nxyz], *qxp = new double[pf.nxyz], *qyp = new double[pf.nxyz];
     double *irho = new double[pf.nxyz], *iux = new double[pf.nxyz], *iuy = new double[pf.nxyz], *imx = new double[pf.nxyz], *imy = new double[pf.nxyz], *iuxp = new double[pf.nxyz], *iuyp = new double[pf.nxyz];
     double *item = new double[pg.nxyz], *iqx = new double[pg.nxyz], *iqy = new double[pg.nxyz], *iqxp = new double[pg.nxyz], *iqyp = new double[pg.nxyz];
     for (int idx = 0; idx < pf.nxyz; idx++) {
-        rho[idx] = 1.0;  ux[idx] = 0.0;  uy[idx] = 0.0;  uxp[idx] = 0.0;  uyp[idx] = 0.0;  tem[idx] = 0.5*(Th + Tl);  qx[idx] = 0.0;  qy[idx] = 0.0;   qxp[idx] = 0.0; qyp[idx] = 0.0;
-        irho[idx] = 0.0; iux[idx] = 0.0; iuy[idx] = 0.0; iuxp[idx] = 0.0; iuyp[idx] = 0.0; imx[idx] = 0.0; imy[idx] = 0.0; item[idx] = 0.0; iqx[idx] = 0.0; iqy[idx] = 0.0; iqxp[idx] = 0.0; iqyp[idx] = 0.0;
+        rho[idx] = 1.0;   ux[idx] = 0.0;    uy[idx] = 0.0;    uxp[idx] = 0.0;   uyp[idx] = 0.0;   tem[idx] = 0.0;   qx[idx] = 0.0;    qy[idx] = 0.0;    qxp[idx] = 0.0; qyp[idx] = 0.0;
+        irho[idx] = 0.0;  iux[idx] = 0.0;   iuy[idx] = 0.0;   iuxp[idx] = 0.0;  iuyp[idx] = 0.0;  imx[idx] = 0.0;   imy[idx] = 0.0;   item[idx] = 0.0;  iqx[idx] = 0.0;   iqy[idx] = 0.0;  iqxp[idx] = 0.0;   iqyp[idx] = 0.0;
     }
     double *alpha = new double[pf.nxyz], *diffusivity = new double[pf.nxyz], *dads = new double[pf.nxyz], *dkds = new double[pf.nxyz];
     double *gi = new double[pg.nxyz*pg.nc], *igi = new double[pg.nxyz*pg.nc];
-    double *directionx = new double[pf.nxyz], *directiony = new double[pf.nxyz];
+    
+    if (MyRank == 0) {
+        std::cout << "U:" << U << std::endl;
+        std::cout << "gy:" << gy << std::endl;
+    }
+    
+    std::vector<double> s(pf.nxyz, 1.0), snm1(pf.nxyz, 1.0);
     for (int i = 0; i < pf.nx; ++i) {
         for (int j = 0; j < pf.ny; ++j) {
-            int idx = pf.Index(i, j);
-            directionx[idx] = ((i + pf.offsetx) == lx/2 && (j + pf.offsety) > 9*ly/10) ? -1.0 : 0.0;
-            directiony[idx] = 0.0;
+            if ((i + pf.offsetx) < mx && (j + pf.offsety) < my) {
+                int idx = pf.Index(i, j);
+                s[idx] = s0;
+                snm1[idx] = s0;
+            }
         }
     }
-
-    std::vector<double> s(pf.nxyz, 1.0), snm1(pf.nxyz, 1.0);
     MMA<double> optimizer(s.size(), 1, 1.0,
 		std::vector<double>(1, 0.0),
 		std::vector<double>(1, 10000.0),
@@ -70,6 +78,8 @@ int main(int argc, char** argv) {
         std::vector<double>(s.size(), 1.0)
     );
     optimizer.move = movelimit;
+
+    std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
 
     for (int k = 1; k <= nk; k++) {
         if (k%nb == 0) {
@@ -81,7 +91,7 @@ int main(int argc, char** argv) {
         for (int i = 0; i < pf.nx; ++i) {
             for (int j = 0; j < pf.ny; ++j) {
                 int idx = pf.Index(i, j);
-                ss[idx] = (j + pf.offsety) < ly/2 ? ss[idx] : 1.0;
+                ss[idx] = ((i + pf.offsetx) < mx && (j + pf.offsety) < my) ? ss[idx] : 1.0;
             }
         }
         
@@ -98,10 +108,10 @@ int main(int argc, char** argv) {
         std::vector<double> dgdss(s.size(), 0.0);
         for (int i = 0; i < pf.nx; ++i) {
             for (int j = 0; j < pf.ny; ++j) {
-                if ((j + pf.offsety) < ly/2) {
+                if ((i + pf.offsetx) < mx && (j + pf.offsety) < my) {
                     int idx = pf.Index(i, j);
-                    g_buffer += ss[idx]/(weightlimit*lx*ly/2);
-                    dgdss[idx] = 1.0/(weightlimit*lx*ly/2); 
+                    g_buffer += (1.0 - ss[idx])/(weightlimit*mx*my);
+                    dgdss[idx] = -1.0/(weightlimit*mx*my); 
                 }
             }
         }
@@ -111,127 +121,92 @@ int main(int argc, char** argv) {
         g = g_buffer;
 #endif
         g -= 1.0;
-
-        //********************Direct analyze********************
+        
+        //********************Objective function********************
+        //  Direct analyse
         int td = nt;
         if (MyRank == 0) {
             std::cout << "Direct analyse t = 0";
         }
         NS::InitialCondition(pf, rho, ux, uy);
         AD::InitialCondition(pg, tem, ux, uy);
-        for (int t = 1; t <= nt0 + nt; ++t) {
-            if (t%dt == 0 && MyRank == 0) {
-                std::cout << "\rDirect analyse t = " << t << std::string(10, ' ');
+        for (int t = 1; t <= nt; t++) {
+            AD::MacroBrinkmanCollideNaturalConvection(pf, rho, ux, uy, alpha, nu, pg, tem, qx, qy, diffusivity, gx, gy, tem0, true, gi);
+            if (t%dt == 0) {
+                if (MyRank == 0) {
+                    std::cout << "\rDirect analyse t = " << t << std::string(10, ' ');
+                }
                 if (Residual(ux, uy, uxp, uyp, pf.nxyz) < eps && Residual(qx, qy, qxp, qyp, pf.nxyz) < eps) {
                     td = t;
                     break;
                 }
             }
-            AD::MacroBrinkmanCollideNaturalConvection(
-                pf, rho, ux, uy, alpha, viscosity, 
-                pg, tem, qx, qy, diffusivity, gx, gy, 0.5*(Th + Tl), true, gi
-            );
-
             pf.Stream();
             pg.Stream();
-            pf.BoundaryCondition([=](int _i, int _j) { return 1; });
-            pg.BoundaryCondition([=](int _i, int _j) { return 0; });
+            pf.BoundaryCondition([=](int _i, int _j) { return _i == 0 ? 2 : 1; });
             AD::BoundaryConditionSetT(pg, 
-                [=](int _i, int _j) { return _i == 0 ? Th : Tl; }, 
+                [=](int _i, int _j) { return tem0; }, 
                 ux, uy, 
-                [=](int _i, int _j) { return (_i == 0 && _j < ly/2) || (_i == lx - 1 && _j < ly/2); }
+                [=](int _i, int _j) { return _i == lx - 1 || _j == ly - 1; }
             );
             AD::BoundaryConditionSetQ(pg, 
-                [=](int _i, int _j) { return 0.0; },
-                ux, uy, diffusivity, 
-                [=](int _i, int _j) { return (_i == 0 && ly/2 <= _j) || (_i == lx - 1 && ly/2 <= _j) || _j == 0 || _j == ly - 1; }
+                [=](int _i, int _j) { return (_j == 0 && _i < L) ? qn : 0.0; }, 
+                ux, uy, diffusivity,
+                [=](int _i, int _j) { return _j == 0; } 
             );
+            pg.BoundaryCondition([=](int _i, int _j) { return _i == 0 ? 2 : 0; });
             pf.SmoothCorner();
             pg.SmoothCorner();
-
-            pf.BoundaryConditionAlongXEdge(lx/5, 1, [=](int _i, int _j) { return ly/2 <= _j && _j < 9*ly/10; });
-            pf.BoundaryConditionAlongYEdge(ly/2, 1, [=](int _i, int _j) { return lx/5 <= _i && _i < 4*lx/5; });
-            pf.BoundaryConditionAlongXEdge(4*lx/5, -1, [=](int _i, int _j) { return ly/2 <= _j && _j < 9*ly/10; });
-            pf.BoundaryConditionAlongYEdge(9*ly/10, 1, [=](int _i, int _j) { return lx/5 <= _i && _i < 4*lx/5; });
-            pf.SmoothCornerAt(lx/5, ly/2, -1, -1);
-            pf.SmoothCornerAt(4*lx/5, ly/2, 1, -1);
-            pf.SmoothCornerAt(4*lx/5, 9*ly/10, 1, 1);
-            pf.SmoothCornerAt(lx/5, 9*ly/10, -1, 1);
-            AD::BoundaryConditionSetQAlongXEdge(pg, lx/5, 1, [=](int _i, int _j) { return 0.0; }, ux, uy, diffusivity, [=](int _i, int _j) { return ly/2 <= _j && _j < 9*ly/10; });
-            AD::BoundaryConditionSetQAlongYEdge(pg, ly/2, 1, [=](int _i, int _j) { return 0.0; }, ux, uy, diffusivity, [=](int _i, int _j) { return lx/5 <= _i && _i < 4*lx/5; });
-            AD::BoundaryConditionSetQAlongXEdge(pg, 4*lx/5, -1, [=](int _i, int _j) { return 0.0; }, ux, uy, diffusivity, [=](int _i, int _j) { return ly/2 <= _j && _j < 9*ly/10; });
-            AD::BoundaryConditionSetQAlongYEdge(pg, 9*ly/10, 1, [=](int _i, int _j) { return 0.0; }, ux, uy, diffusivity, [=](int _i, int _j) { return lx/5 <= _i && _i < 4*lx/5; });
-            pg.SmoothCornerAt(lx/5, ly/2, -1, -1);
-            pg.SmoothCornerAt(4*lx/5, ly/2, 1, -1);
-            pg.SmoothCornerAt(4*lx/5, 9*ly/10, 1, 1);
-            pg.SmoothCornerAt(lx/5, 9*ly/10, -1, 1);
 
             std::swap(ux, uxp);
             std::swap(uy, uyp);
             std::swap(qx, qxp);
             std::swap(qy, qyp);
         }
-
-        //********************Inverse analyze********************
+                
+        //  Inverse analyse
         int ti = nt;
         if (MyRank == 0) {
             std::cout << "\rInverse analyse t = 0" << std::string(10, ' ');
         }
         ANS::InitialCondition(pf, ux, uy, irho, iux, iuy);
         AAD::InitialCondition(pg, ux, uy, item, iqx, iqy);
-        for (int t = 1; t <= nt0 + nt; ++t) {
-            if (t%dt == 0 && MyRank == 0) {
-                std::cout << "\rInverse analyse t = " << t << std::string(10, ' ');
+        for (int t = 1; t <= nt; t++) {
+            AAD::MacroBrinkmanCollideNaturalConvection(
+                pf, rho, ux, uy, irho, iux, iuy, imx, imy, alpha, nu, 
+                pg, tem, item, iqx, iqy, diffusivity,
+                gx, gy, true, igi
+            );
+            if (t%dt == 0) {
+                if (MyRank == 0) {
+                    std::cout << "\rInverse analyse t = " << t << std::string(10, ' ');
+                }
                 if (Residual(iux, iuy, iuxp, iuyp, pf.nxyz) < eps && Residual(iqx, iqy, iqxp, iqyp, pf.nxyz) < eps) {
                     ti = t;
                     break;
                 }
             }
-            AAD::MacroBrinkmanCollideNaturalConvectionMassFlow(
-                pf, rho, ux, uy, irho, iux, iuy, imx, imy, alpha, viscosity,
-                pg, tem, item, iqx, iqy, diffusivity, gx, gy, 
-                directionx, directiony, true, igi
-            );
-
             pf.iStream();
             pg.iStream();
-            pf.iBoundaryCondition([=](int _i, int _j) { return 1; });
-            pg.iBoundaryCondition([=](int _i, int _j) { return 0; });
-            AAD::iBoundaryConditionSetT(pg, ux, uy, [=](int _i, int _j) { return (_i == 0 && _j < ly/2) || (_i == lx - 1 && _j < ly/2); });
-            AAD::iBoundaryConditionSetQ(pg, ux, uy, [=](int _i, int _j) { return (_i == 0 && ly/2 <= _j) || (_i == lx - 1 && ly/2 <= _j) || _j == 0 || _j == ly - 1; });
+            AAD::iBoundaryConditionSetT(pg, ux, uy, [=](int _i, int _j) { return _i == lx - 1 || _j == ly - 1; });
+            AAD::iBoundaryConditionSetQ(pg, ux, uy, [=](int _i, int _j) { return _j == 0; });
+            AAD::iBoundaryConditionSetQ(pg, ux, uy, [=](int _i, int _j) { return _j == 0 && _i < L; }, 1.0);
+            pg.iBoundaryCondition([=](int _i, int _j) { return _i == 0 ? 2 : 0; });
+            pf.iBoundaryCondition([=](int _i, int _j) { return _i == 0 ? 2 : 1; });
             pf.SmoothCorner();
             pg.SmoothCorner();
-
-            pf.iBoundaryConditionAlongXEdge(lx/5, 1, [=](int _i, int _j) { return ly/2 <= _j && _j < 9*ly/10; });
-            pf.iBoundaryConditionAlongYEdge(ly/2, 1, [=](int _i, int _j) { return lx/5 <= _i && _i < 4*lx/5; });
-            pf.iBoundaryConditionAlongXEdge(4*lx/5, -1, [=](int _i, int _j) { return ly/2 <= _j && _j < 9*ly/10; });
-            pf.iBoundaryConditionAlongYEdge(9*ly/10, 1, [=](int _i, int _j) { return lx/5 <= _i && _i < 4*lx/5; });
-            pf.SmoothCornerAt(lx/5, ly/2, -1, -1);
-            pf.SmoothCornerAt(4*lx/5, ly/2, 1, -1);
-            pf.SmoothCornerAt(4*lx/5, 9*ly/10, 1, 1);
-            pf.SmoothCornerAt(lx/5, 9*ly/10, -1, 1);
-            AAD::iBoundaryConditionSetQAlongXEdge(pg, lx/5, 1, ux, uy, [=](int _i, int _j) { return ly/2 <= _j && _j < 9*ly/10; });
-            AAD::iBoundaryConditionSetQAlongYEdge(pg, ly/2, 1, ux, uy, [=](int _i, int _j) { return lx/5 <= _i && _i < 4*lx/5; });
-            AAD::iBoundaryConditionSetQAlongXEdge(pg, 4*lx/5, -1, ux, uy, [=](int _i, int _j) { return ly/2 <= _j && _j < 9*ly/10; });
-            AAD::iBoundaryConditionSetQAlongYEdge(pg, 9*ly/10, 1, ux, uy, [=](int _i, int _j) { return lx/5 <= _i && _i < 4*lx/5; });
-            pg.SmoothCornerAt(lx/5, ly/2, -1, -1);
-            pg.SmoothCornerAt(4*lx/5, ly/2, 1, -1);
-            pg.SmoothCornerAt(4*lx/5, 9*ly/10, 1, 1);
-            pg.SmoothCornerAt(lx/5, 9*ly/10, -1, 1);
 
             std::swap(iux, iuxp);
             std::swap(iuy, iuyp);
             std::swap(iqx, iqxp);
             std::swap(iqy, iqyp);
         }
-
-        //********************Get sensitivity********************
+               
+        //  Get sensitivity
         double f_buffer = 0.0, f;
-        for (int j = 0; j < pf.ny; ++j) {
-            int i = lx/2 - pf.offsetx;
-            if (0 <= i && i < pf.nx && (j + pf.offsety) > 9*ly/10) {
-                int idx = pf.Index(i, j);
-                f_buffer += ux[idx]*directionx[idx] + uy[idx]*directiony[idx];
+        for (int i = 0; i < pf.nx; ++i) {
+            if ((i + pf.offsetx) < L && pf.PEy == 0) {
+                f_buffer += tem[pf.Index(i, 0)];
             }
         }
         #ifdef _USE_MPI_DEFINES
@@ -239,20 +214,25 @@ int main(int argc, char** argv) {
 #else
         f = f_buffer;
 #endif
-        f /= (double)(ly/10);
+        f /= L;
         std::vector<double> dfdss(s.size(), 0.0);
-        AAD::SensitivityBrinkmanDiffusivity(pg, dfdss.data(), ux, uy, imx, imy, dads, tem, item, iqx, iqy, gi, igi, diffusivity, dkds);
-        for (int i = 0; i < pf.nx; ++i) {
-            for (int j = 0; j < pf.ny; ++j) {
-                int idx = pf.Index(i, j);
-                dfdss[idx] = (j + pf.offsety) < ly/2 ? dfdss[idx] : 0.0;
-            }
-        }
-        Normalize(dfdss.data(), pg.nxyz);
-
+        AAD::SensitivityTemperatureAtHeatSource(
+            pg, dfdss.data(), ux, uy, imx, imy, dads, tem, item, iqx, iqy, gi, igi, diffusivity, dkds,
+            [=](int _i, int _j) { return (_j == 0 && _i < L) ? qn : 0.0; }, 
+            [=](int _i, int _j) { return _j == 0 && _i < L; }
+        );
+        Normalize(dfdss.data(), pf.nxyz);
+        
         //********************Filter sensitivities********************
         std::vector<double> dfds = DensityFilter::GetFilteredValue(pf, R, dfdss);
         std::vector<double> dgds = DensityFilter::GetFilteredValue(pf, R, dgdss);
+        for (int i = 0; i < pf.nx; ++i) {
+            for (int j = 0; j < pf.ny; ++j) {
+                int idx = pf.Index(i, j);
+                dfds[idx] = ((i + pf.offsetx) < mx && (j + pf.offsety) < my) ? dfds[idx] : 0.0;
+                dgds[idx] = ((i + pf.offsetx) < mx && (j + pf.offsety) < my) ? dgds[idx] : 0.0;
+            }
+        }
 
         //********************Check grayscale********************
         double mnd_buffer = 0.0, mnd;
@@ -266,7 +246,7 @@ int main(int argc, char** argv) {
 #else
         mnd = mnd_buffer;
 #endif
-        mnd *= 4.0/(double)(lx*ly/2)*100.0;
+        mnd *= 4.0/(double)(mx*my)*100.0;
 
         //********************Update design variable********************
         if (MyRank == 0) {
@@ -278,7 +258,7 @@ int main(int argc, char** argv) {
         for (int i = 0; i < pf.nx; ++i) {
             for (int j = 0; j < pf.ny; ++j) {
                 int idx = pf.Index(i, j);
-                s[idx] = (j + pf.offsety) < ly/2 ? s[idx] : 1.0;
+                s[idx] = ((i + pf.offsetx) < mx && (j + pf.offsety) < my) ? s[idx] : 1.0;
                 double tmpds = fabs(s[idx] - snm1[idx]);
                 if (dsmax_buffer < tmpds) {
                     dsmax_buffer = tmpds;
@@ -300,9 +280,9 @@ int main(int argc, char** argv) {
 
         //********************Check convergence********************
         if (MyRank == 0) {
-            std::cout << "\r" << k << std::scientific << std::setprecision(6) << " " << f << std::fixed << std::setprecision(6) << " " << g << " " << td << " " << ti << " " << dsmax  << " (" << imax << "," << jmax << ") " << qf << " " << qg << " " << mnd << std::endl;
+            std::cout << "\r" << std::fixed << std::setprecision(6) << k << " " << f << " " << g << " " << td << " " << ti << " " << dsmax  << " (" << imax << "," << jmax << ") " << qf << " " << qg << " " << mnd << std::endl;
         }
-        if (dsmax < 0.01 || k == nk) {
+        if ((k > 1 && dsmax < 0.01 && g <= 0.0) || k == nk) {
             if (MyRank == 0) {
                 std::cout << "----------Convergence/Last step----------" << std::endl;
             }
@@ -379,14 +359,15 @@ int main(int argc, char** argv) {
             break;
         }
     }
-
+    
     delete[] rho;   delete[] ux;    delete[] uy;    delete[] uxp;   delete[] uyp;
     delete[] tem;   delete[] qx;    delete[] qy;    delete[] qxp;   delete[] qyp;
     delete[] diffusivity;   delete[] alpha; delete[] dkds;  delete[] dads;
     delete[] irho;  delete[] iux;   delete[] iuy;   delete[] iuxp;  delete[] iuyp;  delete[] imx;   delete[] imy;
     delete[] item;  delete[] iqx;   delete[] iqy;   delete[] iqxp;  delete[] iqyp;
-    delete[] gi;    delete[] igi;   delete[] directionx;    delete[] directiony;
+    delete[] gi;    delete[] igi;
 #ifdef _USE_MPI_DEFINES
     MPI_Finalize();
 #endif
+    return 0;
 }

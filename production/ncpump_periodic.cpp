733d57af2848b38ae8e3ea0f29c0ac28dce6b409
PANFACTORY <yutatanabe510@gmail.com>
2021-10-31 15:38:37 +0900

    Update ncpump_periodic.cpp

--- production/ncpump_periodic.cpp
+++ production/ncpump_periodic.cpp
@@ -39,7 +39,7 @@ int main(int argc, char** argv) {
 
     //--------------------Set parameters--------------------
     int lx = 51, ly = 101, dt = 1000, nt0 = 1000000, nt = 50000, period = 50000, nk = 2000, nb = 100, duty = 20;
-    double viscosity = 0.1/6.0, diff_fluid = viscosity/1.0, Th = 1.0, Tl = 0.0, gx = 0.0, gy = 1000*pow(viscosity, 2)/(double)pow(lx - 1, 3)
+    double viscosity = 0.1/6.0, diff_fluid = viscosity/1.0, Th = 1.0, Tl = 0.0, gx = 0.0, gy = 1000*pow(viscosity, 2)/(double)pow(lx - 1, 3);
     double alphamax = 1e5, diff_solid = diff_fluid*10.0, qf = 1e-6, qg = 1e-4, weightlimit = 0.5, movelimit = 0.2, R = 1.5, eps = 1e-5;
     D2Q9<double> pf(lx, ly, MyRank, mx, my), pg(lx, ly, MyRank, mx, my);
     double **rho = new double*[nt], **ux = new double*[nt], **uy = new double*[nt];

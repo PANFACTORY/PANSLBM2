928cef768c91319e6fa954257e5df7122cf3d3cc
PANFACTORY <yutatanabe510@gmail.com>
2021-11-07 09:16:14 +0900

    Update heatsink3D_transient.cpp

--- production/heatsink3D_transient.cpp
+++ production/heatsink3D_transient.cpp
@@ -333,7 +333,7 @@ int main(int argc, char** argv) {
             file.AddPointData(pf, "ss", [&](int _i, int _j, int _k) { return ss[pf.Index(_i, _j, _k)]; });
             file.AddPointData(pf, "dfdss", [&](int _i, int _j, int _k) { return dfdss[pf.Index(_i, _j, _k)]; });
 #else
-            VTKExport file("result/heatsink_transient3D.vtk", lx, ly);
+            VTKExport file("result/heatsink_transient3D.vtk", lx, ly, lz);
             file.AddPointScaler("rho", [&](int _i, int _j, int _k) { return rho[nt - 1][pf.Index(_i, _j, _k)]; });
             file.AddPointVector("u", 
                 [&](int _i, int _j, int _k) { return ux[nt - 1][pf.Index(_i, _j, _k)]; },

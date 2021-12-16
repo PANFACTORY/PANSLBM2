5524760cb17826e4d6d585414a3b3a04c5ba440c
PANFACTORY <yutatanabe510@gmail.com>
2021-12-16 11:41:09 +0900

    Update vtkxmlimport.h

--- src/utility/vtkxmlimport.h
+++ src/utility/vtkxmlimport.h
@@ -49,16 +49,27 @@ public:
 
         template<class T, template<class>class P>
         void GetPointScalar(P<T>& _p, std::string _label, T *_v);
+        template<class T, template<class>class P>
+        void GetPointVector(P<T>& _p, std::string _label, T *_vx, T *_vy);
+        template<class T, template<class>class P>
+        void GetPointVector(P<T>& _p, std::string _label, T *_vx, T *_vy, T *_vz);
+        template<class T, template<class>class P>
+        void GetPointTensor(P<T>& _p, std::string _label, T *_vxx, T *_vxy, T *_vyx, T *_vyy);
+        template<class T, template<class>class P>
+        void GetPointTensor(P<T>& _p, std::string _label, T *_vxx, T *_vxy, T *_vxz, T *_vyx, T *_vyy, T *_vyz, T *_vzx, T *_vzy, T *_vzz);
 
 private:
         std::ifstream fin_pvts;
         int nx, ny, nz;
         std::string fpath;
         std::vector<std::string> fname_vts;
+
+        template<class F>
+        void GetPointBase(std::string _label, F _f);
     };
 
-    template<class T, template<class>class P>
-    void VTKXMLImport::GetPointScalar(P<T>& _p, std::string _label, T *_v) {
+    template<class F>
+    void VTKXMLImport::GetPointBase(std::string _label, F _f) {
         for (const auto& fname : this->fname_vts) {
             std::ifstream fin_vts(this->fpath + fname);
             std::string tmp;
@@ -81,13 +92,54 @@ private:
                     for (int k = nzmin; k <= nzmax; ++k) {
                         for (int j = nymin; j <= nymax; ++j) {
                             for (int i = nxmin; i <= nxmax; ++i) {
-                                int idx = _p.Index(i, j, k);
-                                fin_vts >> _v[idx];
+                                _f(i, j, k, fin_vts);
                             }
                         }    
                     }
                 }
             }
         }
+    }
+
+    template<class T, template<class>class P>
+    void VTKXMLImport::GetPointScalar(P<T>& _p, std::string _label, T *_v) {
+        this->GetPointBase(_label, [&](int _i, int _j, int _k, std::ifstream& _fin_vts) {
+            int idx = _p.Index(_i, _j, _k);
+            _fin_vts >> _v[idx];
+        });
+    }
+
+    template<class T, template<class>class P>
+    void VTKXMLImport::GetPointVector(P<T>& _p, std::string _label, T *_vx, T *_vy) {
+        T tmp;
+        this->GetPointBase(_label, [&](int _i, int _j, int _k, std::ifstream& _fin_vts) {
+            int idx = _p.Index(_i, _j, _k);
+            _fin_vts >> _vx[idx] >> _vy[idx] >> tmp;
+        });
+    }
+
+    template<class T, template<class>class P>
+    void VTKXMLImport::GetPointVector(P<T>& _p, std::string _label, T *_vx, T *_vy, T *_vz) {
+        this->GetPointBase(_label, [&](int _i, int _j, int _k, std::ifstream& _fin_vts) {
+            int idx = _p.Index(_i, _j, _k);
+            _fin_vts >> _vx[idx] >> _vy[idx] >> _vz[idx];
+        });
+    }
+
+    template<class T, template<class>class P>
+    void VTKXMLImport::GetPointTensor(P<T>& _p, std::string _label, T *_vxx, T *_vxy, T *_vyx, T *_vyy) {
+        T tmp;
+        this->GetPointBase(_label, [&](int _i, int _j, int _k, std::ifstream& _fin_vts) {
+            int idx = _p.Index(_i, _j, _k);
+            _fin_vts >> _vxx[idx] >> _vxy[idx] >> tmp >> _vyx[idx] >> _vyy[idx] >> tmp >> tmp >> tmp >> tmp;
+        });
+    }
+
+    template<class T, template<class>class P>
+    void VTKXMLImport::GetPointTensor(P<T>& _p, std::string _label, T *_vxx, T *_vxy, T *_vxz, T *_vyx, T *_vyy, T *_vyz, T *_vzx, T *_vzy, T *_vzz) {
+        this->GetPointBase(_label, [&](int _i, int _j, int _k, std::ifstream& _fin_vts) {
+            int idx = _p.Index(_i, _j, _k);
+            _fin_vts >> _vxx[idx] >> _vxy[idx] >> _vxz[idx] >> _vyx[idx] >> _vyy[idx] >> _vyz[idx] >> _vzx[idx] >> _vzy[idx] >> _vzz[idx];
+        });
     }
 }
